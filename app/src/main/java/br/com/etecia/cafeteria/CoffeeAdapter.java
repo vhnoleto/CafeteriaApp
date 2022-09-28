b0166c10346567369a48ee8af35bd14cd2763b5d
vhnoleto <victornoleto43@gmail.com>
2022-09-27 22:08:11 -0300

    Row commit

--- app/src/main/java/br/com/etecia/cafeteria/CoffeeAdapter.java
+++ app/src/main/java/br/com/etecia/cafeteria/CoffeeAdapter.java
@@ -1,10 +1,14 @@
 package br.com.etecia.cafeteria;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -21,12 +25,27 @@ public class CoffeeAdapter extends RecyclerView.Adapter<CoffeeAdapter.ViewHolder
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CoffeeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        view = inflater.inflate(R.layout.modelo, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CoffeeAdapter.ViewHolder holder, int position) {
+        holder.nomeCafe.setText(mData.get(position).getTitulo());
+        holder.fotoCafe.setImageResource(mData.get(position).getFoto());
+        holder.descricaoCafe.setText(mData.get(position).getDesc());
+        holder.favcafe.setImageResource(mData.get(position).getRating());
+        holder.cardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void OnClick(View view){}
+                                           }
+
+
+        );
+
 
     }
 
@@ -36,9 +55,22 @@ public class CoffeeAdapter extends RecyclerView.Adapter<CoffeeAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView nomeCafe;
+        ImageView fotoCafe;
+        TextView descricaoCafe;
+        View notaCafe;
+        ImageView favcafe;
+        CardView cardView;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView)
+        {
             super(itemView);
+            nomeCafe = itemView.findViewById(R.id.cafeTitulo);
+            fotoCafe = itemView.findViewById(R.id.cafeImg);
+            descricaoCafe = itemView.findViewById(R.id.cafeDescricao);
+            notaCafe = itemView.findViewById(R.id.Cafenota);
+            favcafe = itemView.findViewById(R.id.imgfavorite);
+            cardView = itemView.findViewById(R.id.modeloItem);
         }
     }
 
